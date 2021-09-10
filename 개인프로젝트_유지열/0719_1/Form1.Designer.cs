ac3e563db13ca34c1837d823c161a9539bc2a01b
jiyeol1 <90430499+jiyeol1@users.noreply.github.com>
2021-09-10 11:35:40 +0900

    Add files via upload

---
+++ 개인프로젝트_유지열/0719_1/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace _0719_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(-4, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(442, 306);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint_1);
+            this.pictureBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDoubleClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown_1);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(624, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "이미지불러오기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(624, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "이미지 흑백";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(624, 261);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "저장하기";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(624, 202);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(122, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "이미지 복구";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(624, 314);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(122, 124);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "종료하기";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(624, 72);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "좌우반전";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ 개인프로젝트_유지열/0719_1/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Drawing2D;
+
+
+namespace _0719_1
+{
+    public partial class Form1 : Form
+    {
+        private Point LastPoint;
+        private Bitmap img;
+        private double ratio = 1.0F;
+        private Point imgPoint;
+        private Rectangle imgRect;
+        private Point clickPoint;
+        String file_path = "";
+        string img_path;
+        public Form1()
+        {
+            InitializeComponent();
+            ///휠 이벤트 선언
+            pictureBox1.MouseWheel += new MouseEventHandler(pictureBox1_MouseWheel);
+
+
+            imgPoint = new Point(pictureBox1.Width / 2, pictureBox1.Height / 2);
+            imgRect = new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height);
+
+            clickPoint = imgPoint;
+
+            pictureBox1.Invalidate();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog1.InitialDirectory = "C:\\";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                file_path = openFileDialog1.FileName;
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.Image = (Bitmap)Image.FromFile(file_path);
+
+            img_path = file_path;
+
+
+        }
+       private void button4_Click(object sender, EventArgs e)
+        {
+        pictureBox1.Image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            pictureBox1.Refresh();
+        }
+        private void pictureBox1_MouseWheel(object sender, MouseEventArgs e)
+        {
+           
+            int lines = e.Delta * SystemInformation.MouseWheelScrollLines / 120;
+            PictureBox pb = (PictureBox)sender;
+
+            if (lines > 0)
+            {
+                ratio *= 1.1F;
+                if (ratio > 100.0) ratio = 100.0f;
+
+                imgRect.Width = (int)Math.Round(pictureBox1.Width * ratio);
+                imgRect.Height = (int)Math.Round(pictureBox1.Height * ratio);
+                imgRect.X = -(int)Math.Round(1.1F * (imgPoint.X - imgRect.X) - imgPoint.X);
+                imgRect.Y = -(int)Math.Round(1.1F * (imgPoint.Y - imgRect.Y) - imgPoint.Y);
+            }
+            else if (lines < 0)
+            {
+                ratio *= 0.9F;
+                if (ratio < 1) ratio = 1;
+
+                imgRect.Width = (int)Math.Round(pictureBox1.Width * ratio);
+                imgRect.Height = (int)Math.Round(pictureBox1.Height * ratio);
+                imgRect.X = -(int)Math.Round(0.9F * (imgPoint.X - imgRect.X) - imgPoint.X);
+                imgRect.Y = -(int)Math.Round(0.9F * (imgPoint.Y - imgRect.Y) - imgPoint.Y);
+            }
+
+            if (imgRect.X > 0) imgRect.X = 0;
+            if (imgRect.Y > 0) imgRect.Y = 0;
+            if (imgRect.X + imgRect.Width < pictureBox1.Width) imgRect.X = pictureBox1.Width - imgRect.Width;
+            if (imgRect.Y + imgRect.Height < pictureBox1.Height) imgRect.Y = pictureBox1.Height - imgRect.Height;
+            pictureBox1.Invalidate();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+
+        private void pictureBox1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+            if (pictureBox1.Image != null)
+            {
+                e.Graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+                e.Graphics.DrawImage(pictureBox1.Image, imgRect);
+                pictureBox1.Focus();
+            }
+        }
+
+        private void pictureBox1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+
+            if (e.Button == MouseButtons.Left)
+            {
+                clickPoint = new Point(e.X, e.Y);
+            }
+            pictureBox1.Invalidate();
+        }
+
+
+        private void pictureBox1_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                imgRect.X = imgRect.X + (int)Math.Round((double)(e.X - clickPoint.X) / 5);
+                if (imgRect.X >= 0) imgRect.X = 0;
+                if (Math.Abs(imgRect.X) >= Math.Abs(imgRect.Width - pictureBox1.Width))
+                    imgRect.X = -(imgRect.Width - pictureBox1.Width);
+                imgRect.Y = imgRect.Y + (int)Math.Round((double)(e.Y - clickPoint.Y) / 5);
+                if (imgRect.Y >= 0) imgRect.Y = 0;
+                if (Math.Abs(imgRect.Y) >= Math.Abs(imgRect.Height - pictureBox1.Height)) imgRect.Y = -(imgRect.Height - pictureBox1.Height);
+            }
+            else
+            {
+                LastPoint = e.Location;
+            }
+
+            pictureBox1.Invalidate();
+        }
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            pictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+    
+            pictureBox1.Refresh();
+        }
+
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            img = new Bitmap((Bitmap)pictureBox1.Image);
+            Color col;
+            int Average;
+            
+            //x 는 image의 width
+            //y 는 image의 hediht
+            for (int x = 0; x < img.Width; x++)
+            {
+                for (int y = 0; y < img.Height; y++)
+                {
+
+                    col = img.GetPixel(x, y);
+                    Average = (col.R + col.G+ col.B) / 3;
+
+                    col = Color.FromArgb(Average, Average, Average);
+                    img.SetPixel(x, y, col);
+                }
+            }
+
+            pictureBox1.Image = img;
+
+
+
+        }
+       
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Title = "다른 이름으로 저장";
+            dlg.DefaultExt = "jpg";
+            dlg.Filter = "JPEG (*.jpg)|*.jpg|Bitmap (*.bmp)|*.bmp|GIF (*.gif)|*.gif";
+            dlg.FilterIndex = 0;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(dlg.FileName);
+            }
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           
+         
+            pictureBox1.Image = (Bitmap)Image.FromFile(img_path);
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        
+    }
+}
+
